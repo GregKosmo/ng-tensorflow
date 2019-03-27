01be722529d11372f8525f0758217951101de48d
Greg <greg@gregkosmacek.com>
2019-03-26 23:28:06 -0500

    Start converting to more human friendly language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as tf from '@tensorflow/tfjs';
-import { Sequential, History, Tensor, Rank } from '@tensorflow/tfjs';
+import { Sequential, Tensor, Rank, History } from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    trainingResults: History;
+export class AppComponent implements OnInit {
     model: Sequential;
-    data: Car[];
-    tensorData: TensorData;
+    sourceData: any[];
+    dataProperties: string[];
+    dataLoaded: boolean;
+    numberOfSourceRecords: number;
+    modelTaught: boolean;
 
-    async runTraining() {
-        this.data = await this.getCars();
-        this.model = this.createModel();
+    learnProperty: string;
+    estimateProperty: string;
+    testBatchSize = 28;
+    testIterations = 50;
+    normalizedInputs: Tensor<Rank>;
+    normalizedLabels: Tensor<Rank>;
+    inputMax: Tensor<Rank>;
+    inputMin: Tensor<Rank>;
+    labelMax: Tensor<Rank>;
+    labelMin: Tensor<Rank>;
 
-        this.tensorData = this.convertToTensor(this.data);
-        this.trainingResults = await this.trainModel(this.model, this.tensorData.inputs, this.tensorData.labels)
-
-        console.log('Training Results:');
-        console.log(this.trainingResults);
-        console.log('');
+    ngOnInit() {
+        this.createModel();
     }
 
-    runTesting() {
-        this.testModel(this.model, this.data, this.tensorData);
+    readSource(sourceFiles: File[]) {
+        let reader = new FileReader();
+        reader.addEventListener('load', () => {
+            if(typeof reader.result === 'string') {
+                this.sourceData = JSON.parse(reader.result);
+                this.numberOfSourceRecords = this.sourceData.length;
+
+                this.dataProperties = [];
+                for(let obj of this.sourceData) {
+                    for(let prop in obj) {
+                        if(this.dataProperties.indexOf(prop) === -1) {
+                            this.dataProperties.push(prop);
+                        }
+                    }
+                }
+            }
+
+            this.dataLoaded = true;
+        });
+        reader.readAsText(sourceFiles[0]);
     }
 
-    async getCars(): Promise<Car[]> {
-        let request = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
-        let carData: any[] = await request.json();
-        let cars: Car[] = carData.map(car => {
-            let newCar = new Car();
-            newCar.mpg = car.Miles_per_Gallon;
-            newCar.horsepower = car.Horsepower;
-            return newCar;
-        })
-        .filter(car => (car.mpg !== null && car.horsepower !== null));
-
-        return cars;
+    async teachModel() {
+        this.setTensors(this.sourceData);
+        await this.trainModel();
+        this.modelTaught = true;
     }
 
-    createModel(): Sequential {
+    createModel() {
         let model = tf.sequential();
         model.add(tf.layers.dense({
             inputShape: [1], 
@@ -56,69 +71,55 @@ export class AppComponent {
             useBias: true
         }));
 
-        return model;
+        this.model = model;
     }
 
-    convertToTensor(data: Car[]): TensorData {
+    setTensors(data: any[]): any {
         return tf.tidy(() => {
             tf.util.shuffle(data);
 
-            let inputs = data.map(d => d.horsepower);
-            let labels = data.map(d => d.mpg);
+            let inputs = data.map(d => d[this.learnProperty]);
+            let labels = data.map(d => d[this.estimateProperty]);
 
             let inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
             let labelTensor = tf.tensor2d(labels, [labels.length, 1]);
 
-            let inputMax = inputTensor.max();
-            let inputMin = inputTensor.min();
-            let labelMax = labelTensor.max();
-            let labelMin = labelTensor.min();
-
-            let normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
-            let normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
-
-            return {
-                inputs: normalizedInputs,
-                labels: normalizedLabels,
-                inputMax,
-                inputMin,
-                labelMax,
-                labelMin,
-            }
+            this.inputMax = inputTensor.max();
+            this.inputMin = inputTensor.min();
+            this.labelMax = labelTensor.max();
+            this.labelMin = labelTensor.min();
+            this.normalizedInputs = inputTensor.sub(this.inputMin).div(this.inputMax.sub(this.inputMin));
+            this.normalizedLabels = labelTensor.sub(this.labelMin).div(this.labelMax.sub(this.labelMin));
         })
     }
 
-    async trainModel(model: Sequential, inputs: Tensor<Rank>, labels: Tensor<Rank>): Promise<History> {
-        model.compile({
+    async trainModel(): Promise<History> {
+        this.model.compile({
             optimizer: tf.train.adam(),
             loss: tf.losses.meanSquaredError,
             metrics: ['mse']
         });
 
-        let batchSize = 28;
-        let epochs = 50;
-
-        return await model.fit(inputs, labels, {
-            batchSize,
-            epochs,
+        return await this.model.fit(this.normalizedInputs, this.normalizedLabels, {
+            batchSize: this.testBatchSize,
+            epochs: this.testIterations,
             shuffle: true
         })
     }
 
-    testModel(model: Sequential, inputData: Car[], normalizationData: TensorData) {
-
+    testModel() {
         let data = tf.tidy(() => {
             let xs = tf.linspace(0, 1, 100);
-            let preds = model.predict(xs.reshape([100, 1]));
+            let preds = this.model.predict(xs.reshape([100, 1]));
 
             if(!(preds instanceof Array)) {
                 let unNormXs = xs
-                    .mul(normalizationData.inputMax.sub(normalizationData.inputMin))
-                    .add(normalizationData.inputMin);
+                    .mul(this.inputMax.sub(this.inputMin))
+                    .add(this.inputMin);
 
                 let unNormPreds = preds
-                    .mul(normalizationData.labelMax.sub(normalizationData.labelMin))
-                    .add(normalizationData.labelMin);
+                    .mul(this.labelMax.sub(this.labelMin))
+                    .add(this.labelMin);
 
                 return {
                     xs: unNormXs.dataSync(), 
@@ -130,29 +131,5 @@ export class AppComponent {
         let predictedPoints = Array.from(data.xs).map((val, i) => {
             return {x: val, y: data.preds[i]};
         });
-
-        let originalPoints = inputData.map(d => ({
-            x: d.horsepower, y: d.mpg
-        }));
-
-        console.log('Predicted Points:');
-        console.log(predictedPoints);
-        console.log('');
-        console.log('Original Points');
-        console.log(originalPoints);
     }
-}
-
-export class Car {
-    horsepower: number;
-    mpg: number
-}
-
-export class TensorData {
-    inputs: Tensor<Rank>;
-    labels: Tensor<Rank>;
-    inputMax: Tensor<Rank>;
-    inputMin: Tensor<Rank>;
-    labelMax: Tensor<Rank>;
-    labelMin: Tensor<Rank>;
 }
